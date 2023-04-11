942dfbeb736da5816ee1f8fbac2d6ad3786e1290
Igor <eltery@mail.ru>
2023-04-11 23:00:55 +0300

    coin task done

--- HomeWork_02/task_10.py
+++ HomeWork_02/task_10.py
@@ -5,3 +5,25 @@
 # количество монет, которые нужно перевернуть.
 # 5 -> 1 0 1 1 0
 # 2
+
+import random
+number_of_coins = int(input("Введите количество монет: "))
+
+coins = []
+for i in range (number_of_coins):
+    side_of_coin = random.randint(0, 1) # генерируем случайное положение монетки - орёл или решка
+    coins.append(side_of_coin)
+print(coins)
+
+counter_of_tails = 0    #счётчик монет, повёрнутых решкой
+counter_of_eagles = 0   #счётчик монет, повёрнутых орлом
+for i in coins:
+    if i == 0:
+        counter_of_tails += 1 
+    else:
+        counter_of_eagles += 1 #Была мысль также просто посчитать количество решек и отнять его от общего количества монет, чтобы получить количество орлов.
+
+if counter_of_tails <= counter_of_eagles:
+    print(f"Необходимо перевернуть {counter_of_tails} монет(-ы)") 
+else:
+    print(f"Необходимо перевернуть {counter_of_eagles} монет(-ы)")
